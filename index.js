af957d29c6a23e6f2981e3e1749665053161914b
asciidisco <public@asciidisco.com>
2013-07-03 17:47:49 +0200

    Refactoring, documenting & bigfixing

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
+/*!
+ *
+ * Copyright (c) 2013 Sebastian Golasch
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+'use strict';
+
+// ext. libs
+var fs = require('fs');
+
 // int. libs
 var WebDriver = require('./lib/webdriver');
 var Driver = require('./lib/driver')(WebDriver);
 
 // include webdriver commands
-require('./lib/commands/session')(Driver);
-require('./lib/commands/timeout')(Driver);
-require('./lib/commands/window')(Driver);
-require('./lib/commands/url')(Driver);
-require('./lib/commands/execute')(Driver);
-require('./lib/commands/screenshot')(Driver);
-require('./lib/commands/ime')(Driver);
-require('./lib/commands/frame')(Driver);
-require('./lib/commands/cookie')(Driver);
-require('./lib/commands/element')(Driver);
-require('./lib/commands/interaction')(Driver);
-require('./lib/commands/storage')(Driver);
-require('./lib/commands/page')(Driver);
+var dir = __dirname + '/lib/commands/';
+fs.readdirSync(dir).forEach(function (file) {
+  require(dir + file)(Driver);
+});
 
 module.exports = WebDriver;
